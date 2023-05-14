9afc310cf1e03a98f8e2960f07e79d2e3048f058
jiml268 <jimlynch268@gmail.com>
2023-05-14 19:02:01 -0400

    added get and set local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,25 @@ import '../styles/styles.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
+
+ componentDidMount() {
+    const fromLocal = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(fromLocal);
+    if (!parsedContacts) return;
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
 
   handleChange = e => {
     const { name, value } = e.target;
@@ -30,7 +39,7 @@ export class App extends Component {
     const contactsLists = [...this.state.contacts];
 
     if (contactsLists.findIndex(contact => name === contact.name && number === contact.number) !== -1) {
-      alert(`${name} is already in contacts.`);
+      alert(`${name} with phone number ${number} is already in contacts.`);
     } else {
       contactsLists.push({ name, id, number });
     }
